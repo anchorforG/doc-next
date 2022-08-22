e2f57aa1cb3eefb8f2be44a309ead2133be9cf78
anchor <ghouxiatian@outlook.com>
2022-08-22 15:24:10 +0800

    test workflow

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,7 +1,27 @@
-import { defineUserConfig } from 'vuepress'
+// import process from 'node:process'
+// import { viteBundler } from '@vuepress/bundler-vite'
+// import { webpackBundler } from '@vuepress/bundler-webpack'
+import { defineUserConfig } from '@vuepress/cli'
+// import { docsearchPlugin } from '@vuepress/plugin-docsearch'
+// import { googleAnalyticsPlugin } from '@vuepress/plugin-google-analytics'
+// import { registerComponentsPlugin } from '@vuepress/plugin-register-components'
+// import { shikiPlugin } from '@vuepress/plugin-shiki'
+import { defaultTheme } from '@vuepress/theme-default'
+// import { getDirname, path } from '@vuepress/utils'
+// import {
+//   head,
+//   navbarZh,
+//   sidebarZh,
+// } from './configs/index.js'
+
+// const __dirname = getDirname(import.meta.url)
+// const isProd = process.env.NODE_ENV === 'production'
 
 export default defineUserConfig({
-  lang: 'zh-CN',
-  title: '你好， VuePress ！',
-  description: '这是我的第一个 VuePress-next 站点',
+  // set site base to default value
+  base: '/doc-next/',
+
+  // extra tags in `<head>`
+  // head,
+
 })
